c7058a9f151eeb64894a53261c938b092bc36cab
MarlonVictor <cmarlonvictor11@gmail.com>
2020-11-23 13:28:26 -0200

    Structured project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+
 const App = () => {
     return (
-        <>
-            <h1>Hello World</h1>
-        </>
+        <div className="max-h-screen overflow-hidden">
+            <h1 className="bg-brown text-white font-extrabold p-3">Hello World</h1>
+        </div>
     )
-};
+}
 
-export default App; 
+export default App;
